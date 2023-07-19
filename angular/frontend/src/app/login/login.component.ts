cacbacf495900b02908b8729d38f5920797f7e3b
mikalikahi <sweeney.phil@gmail.com>
2023-07-19 16:03:17 +0800

    Form in template and functions is ts file for login

--- angular/frontend/src/app/login/login.component.ts
+++ angular/frontend/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../authentication/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isErrorLogin:boolean = false;
+  private errorLogin$: Subscription;
+  errorMsg:string = 'There was an error logging in'
+
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+    this.isErrorLogin = this.authService.getIsAuth();
+    this.errorLogin$ = this.authService
+      .getLoginErrorListener()
+      .subscribe(isErrorLogin => {
+        this.isErrorLogin = isErrorLogin;
+    });
+  }
+
+  onClearLoginError() {
+    this.authService.clearLoginError();
+  }
+
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.authService.login(form.value.username, form.value.password);
+    form.reset();
   }
 
 }
