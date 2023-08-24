9ecf4a1578ab66ac48f0966fe73e2395baa8d504
mikalikahi <sweeney.phil@gmail.com>
2023-08-24 15:41:48 +0800

    Updated deprecated Effects() decorator to instead use createEffect() method

--- angular/frontend/src/app/authenticated-user/user.effects.ts
+++ angular/frontend/src/app/authenticated-user/user.effects.ts
@@ -1,13 +1,14 @@
-import {Injectable} from '@angular/core';
-import {Actions, Effect, ofType} from '@ngrx/effects';
-import {select, Store} from '@ngrx/store';
-import {throwError} from 'rxjs';
-import {catchError, concatMap, exhaustMap, filter, map, mergeMap, switchMap, withLatestFrom} from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { select, Store } from '@ngrx/store';
+import { throwError } from 'rxjs';
+import { catchError, filter, map, 
+  mergeMap, withLatestFrom}  from "rxjs/operators";
 
 import { AppState } from '../reducers';
 import { selectUserProfile } from './user.selectors';
 import { UserService } from './user.service';
-import { UserProfileActions, UserProfileActionTypes, UserProfileSubmitted,
+import { UserProfileActionTypes, UserProfileSubmitted,
     UserProfileLoaded, UserProfileRequested, UserProfileSaved,
      } from './user.actions';
 
@@ -15,8 +16,8 @@ import { UserProfileActions, UserProfileActionTypes, UserProfileSubmitted,
 @Injectable()
     export class UserEffects {
     
-        @Effect()
-        loadUserProfile$ = this.actions$
+      loadUserProfile$ = createEffect(() => {
+        return this.actions$
           .pipe(
             ofType<UserProfileRequested>(UserProfileActionTypes.UserProfileRequested),
             withLatestFrom(this.store.pipe(select(selectUserProfile))),
@@ -25,14 +26,15 @@ import { UserProfileActions, UserProfileActionTypes, UserProfileSubmitted,
             map(usrProfile => new UserProfileLoaded({ usrProfile }),
             catchError(err => {
                 console.log('Error loading user info: ', err);
-                return throwError(err);
+                return throwError(() => err);
               })
             )
           )
+      });
 
-        @Effect()
-        submitEditedProfile$ = this.actions$
-            .pipe(
+      submitEditedProfile$ = createEffect(() => {
+        return this.actions$
+          .pipe(
             ofType<UserProfileSubmitted>(UserProfileActionTypes.UserProfileSubmitted),
             mergeMap(action => this.userService.editUserProfile(action.payload.submissionForm)
                 .pipe(
@@ -40,7 +42,8 @@ import { UserProfileActions, UserProfileActionTypes, UserProfileSubmitted,
                 ),
                 )
             )
+      });
 
-        constructor(private actions$: Actions, private userService: UserService,
-            private store: Store<AppState>) {}
+      constructor(private actions$: Actions, private userService: UserService,
+          private store: Store<AppState>) {}
 }
