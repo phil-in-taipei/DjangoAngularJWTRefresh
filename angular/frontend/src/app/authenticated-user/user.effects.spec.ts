dc7cd05d69576556acc9ed89c632f3f5dfde2a83
mikalikahi <sweeney.phil@gmail.com>
2023-08-31 15:29:03 +0800

    Both approaches of testing user effects ngrx carried out -- the one with the setup and partial mock services is impractical and just for reference

--- angular/frontend/src/app/authenticated-user/user.effects.spec.ts
+++ angular/frontend/src/app/authenticated-user/user.effects.spec.ts
@@ -1,116 +1,94 @@
-import { TestBed } from '@angular/core/testing';
-import { provideMockActions } from '@ngrx/effects/testing';
+import { TestBed, fakeAsync, flush } from '@angular/core/testing';
 import { Action } from '@ngrx/store';
+import { provideMockActions } from '@ngrx/effects/testing';
 import { from, Observable, of, throwError } from 'rxjs';
 import { toArray } from 'rxjs/operators';
 import { provideMockStore } from '@ngrx/store/testing';
-import { initialUserProfileState, UserProfileState } from './user.reducers';
-import { selectUserProfile } from './user.selectors';
+
+
+import { UserEffects } from './user.effects';
+import { initialUserProfileState } from './user.reducers';
+import { 
+  userProfileData, userProfileEdited, userProfileEditData 
+} from '../test-data/authenticated-user-module-tests/user-related-tests/user-data';
 import { 
     UserProfileSubmitted,
     UserProfileLoaded, 
     UserProfileRequested, 
-    UserProfileSaved} from './user.actions';
-import { UserProfileModel } from '../models/user-profile.model';
-
-import { 
-    userProfileData, userProfileEditData, userProfileEdited 
-    } from '../test-data/authenticated-user-module-tests/user-related-tests/user-data';
-import { UserEffects } from './user.effects';
+    UserProfileSaved
+  } from './user.actions';
+import { selectUserProfile } from './user.selectors';
 import { UserService } from './user.service';
+import { UserProfileModel, 
+  UserProfileEditModel } from '../models/user-profile.model';
 
-type PartialUserServiceFetch = Pick<UserService, 'fetchUserProfile'>;
 
-type PartialUserServiceEdit = Pick<UserService, 'editUserProfile'>;
-
-const fakeUserServiceFetch: PartialUserServiceFetch = {
-    fetchUserProfile(): Observable<UserProfileModel> {
-      return of(userProfileData);
-    },
-  };
-
-const fakeUserServiceEdit: PartialUserServiceEdit = {
-    editUserProfile(userProfileEditData): Observable<UserProfileModel> {
-      return of(userProfileEdited);
-    },
-};
-
-function expectActions(effect: Observable<Action>, actions: Action[]): void {
-    let actualActions: Action[] | undefined;
-    effect.pipe(toArray()).subscribe((actualActions2) => {
-      actualActions = actualActions2;
-    }, fail);
-    expect(actualActions).toEqual(actions);
-  }
-
-function setupOne(actions: Action[], userService: PartialUserServiceFetch): UserEffects {
-    spyOn(userService, 'fetchUserProfile').and.callThrough();
+fdescribe('UserEffects', () => {
+    let effects: UserEffects;
+    let userService: UserService;
   
-    TestBed.configureTestingModule({
-      providers: [
-        provideMockStore({
-            initialState: {
-                'user': initialUserProfileState
-            },
-            selectors: [
-                {
-                  selector: selectUserProfile,
-                  value: undefined
-                }
-              ]
-        }),
-        provideMockActions(from(actions)),
-        { provide: UserService, useValue: userService },
-        UserEffects,
-      ],
+    beforeEach(() => {
+      const mockUserService = {
+        fetchUserProfile(): Observable<UserProfileModel> {
+          return of(userProfileData);
+        },
+        editUserProfile(userProfileEditData: UserProfileEditModel): 
+          Observable<UserProfileModel> {
+            return of(userProfileEdited);
+        }
+      };
+
+      TestBed.configureTestingModule({    
+        providers: [
+            provideMockStore({
+                initialState: {
+                    'user': initialUserProfileState
+                },
+                selectors: [
+                    {
+                      selector: selectUserProfile,
+                      value: undefined
+                    }
+                  ]
+            }),
+            provideMockActions(from([new UserProfileRequested(), 
+              new UserProfileSubmitted({ submissionForm: userProfileEditData})])),
+            UserEffects,
+            { provide: UserService, useValue: mockUserService }
+
+          ]
+      });
+
+      effects = TestBed.inject(UserEffects);
+      userService = TestBed.inject(UserService);
     });
-  
-    return TestBed.inject(UserEffects);
-}
+            
+    it('should call fetch the user profile if there is no user in state', 
+        fakeAsync(() => {
+        spyOn(userService, 'fetchUserProfile').and.returnValue(of(userProfileData));
+        let actualActions: Action[] | undefined;
+        const expectedActions: Action[] = [new UserProfileLoaded({ usrProfile: userProfileData })];
+        
+        effects.loadUserProfile$.pipe(toArray()).subscribe((actualActions2) => {
+            actualActions = actualActions2;
+          }, fail);
+        
+        expect(actualActions).toEqual(expectedActions);
+        flush();
+    }));
 
-function setupTwo(actions: Action[], userService: PartialUserServiceEdit): UserEffects {
-    spyOn(userService, 'editUserProfile').and.returnValue(of(userProfileEdited));
-  
-    TestBed.configureTestingModule({
-      providers: [
-        provideMockStore({
-            initialState: {
-                'user': initialUserProfileState
-            },
-            selectors: [
-                {
-                  selector: selectUserProfile,
-                  value: userProfileData
-                }
-              ]
-        }),
-        provideMockActions(from(actions)),
-        { provide: UserService, useValue: userService },
-        UserEffects,
-      ],
-    });
-  
-    return TestBed.inject(UserEffects);
-}
+    it('should submit the edited user data to backend and update user data in state', 
+      fakeAsync(() => {
+      spyOn(userService, 'editUserProfile').and.returnValue(of(userProfileEdited));
+      let actualActions: Action[] | undefined;
+      const expectedActions: Action[] = [new UserProfileSaved({ usrProfile: userProfileEdited })];
+      
+      effects.submitEditedProfile$.pipe(toArray()).subscribe((actualActions2) => {
+          actualActions = actualActions2;
+        }, fail);
+      
+      expect(actualActions).toEqual(expectedActions);
+      flush();
+    }));
 
-describe('UserEffects', () => {
-    it('gets the user profile from backend on request to load if no user is in state', 
-        () => {
-        const userEffects = setupOne([new UserProfileRequested()], fakeUserServiceFetch);
-        expectActions(userEffects.loadUserProfile$, 
-            [new UserProfileLoaded({ usrProfile: userProfileData })]
-        );
-
-    });
-
-    it('gets submits the edited user profile to backend and revised user data in state', 
-        () => {
-        const userEffects = setupTwo(
-            [new UserProfileSubmitted({ submissionForm: userProfileEditData})], 
-            fakeUserServiceEdit);
-        expectActions(userEffects.submitEditedProfile$, 
-            [new UserProfileSaved({ usrProfile: userProfileEdited })]
-        );
-
-    }); 
-});
+  });
